232c1e731a4b237ee9cdf3b47ab710a27946a3c4
Dumindu <dumindu.madunuwan@gmail.com>
2012-05-21 00:32:49 +0530

    still incomplete, 1st phase of design for desktops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(function() {
 	$(".nano").nanoScroller();
+	//$('.thumbs a').touchTouch();
+	window.prettyPrint && prettyPrint();
 });
 function to_top() {
 	$(".nano").nanoScroller({scroll:'top'});
