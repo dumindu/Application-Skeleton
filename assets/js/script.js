13342d664af02e642d115c6ea7bc8f851d7ef2c4
Dumindu <dumindu.madunuwan@gmail.com>
2012-05-27 13:45:02 +0530

    added the functionality to focus a div on load, but not working even on Chrome and Opera

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ $(function() {
 	$(".nano").nanoScroller();
 	//$('.thumbs a').touchTouch();
 	window.prettyPrint && prettyPrint();
+	$("#main_focus").focus();
+
 });
 function to_top() {
 	$(".nano").nanoScroller({scroll:'top'});
